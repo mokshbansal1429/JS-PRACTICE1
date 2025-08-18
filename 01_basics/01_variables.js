fd05f846fed6ab07589640a8ceeef09cc5416d4a
mokshbansal1429 <moksh1429.becse24@chitkara.edu.in>
2025-08-18 07:32:44 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+//const agr rk bar assign kr diya to change nhi kr skte
+const accountId=14432
+let emailId="moksh@google.com"
+/*
+agr var ka use krte h to jha scope ka use hoga vo hr var ki value change kr dega
+*/
+var accountPassword="112233"
+//accountId=344//not be use 
+let accountState;
+/*
+prefer not to use var
+because of issue in block and functional scope
+*/
+accountCity="jaipur"
+console.log(accountId)
+console.table([accountId,emailId,accountPassword,accountCity,accountState])
