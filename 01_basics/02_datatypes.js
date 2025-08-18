456f4ed34c3b7649fdf3386cff571a9d1a3cbd4c
mokshbansal1429 <moksh1429.becse24@chitkara.edu.in>
2025-08-18 07:31:08 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";//use new version of javascript
+//alert(3+3)//we are using node js not browser
+console.log(3+3)
+console.log("moksh")
+let name="moksh"//string datatype
+let age=19 //int datatype
+let isLoggedIn=false //boolean datatype
+let state;
+// number=>2 ki power 53
+// bigint
+//string=> ""
+//boolean=true/false
+// null
+//undefined
+//object
+console.log(typeof "moksh")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
