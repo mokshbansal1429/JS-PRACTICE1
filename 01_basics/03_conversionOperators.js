5d7069215cef9984e05f692c4870be6ea9ce68ca
mokshbansal1429 <moksh1429.becse24@chitkara.edu.in>
2025-08-18 19:15:02 +0000

    conversionOperators

---
+++ 01_basics/03_conversionOperators.js
@@ -0,0 +1,40 @@
+let age=33
+let age1="33"
+// console.log(age)
+// console.log(typeof age)
+// console.log(typeof age1)
+// let valueInNumber=Number(age)
+// console.log(typeof valueInNumber)
+// let isLoggedIn=1
+// let isLoggedIn=""
+let isLoggedIn="moksh"
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+let someNumber=34
+let StringNumber=String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
+
+// **********  OPERATIONS ********
+
+
+// let value=56
+// let negvalue=-value;
+// console.log(negvalue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+let str1="helllo"
+let str2=" moksh"
+let str3= str1 + str2
+console.log(str3)
+console.log("1" + 2 +2)
+console.log(1 + "2")
+console.log(1 + 2 +"3")
+let num1=num2=num3=2+2;
+let gamechanger=100
+gamechanger++
+console.log(gamechanger);
