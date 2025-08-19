8dd3afcc676b28e00166701cbcd26b134db49296
mokshbansal1429 <moksh1429.becse24@chitkara.edu.in>
2025-08-19 07:14:36 +0000

    datatypes-summary

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,30 @@
+//primitive
+// 7 types=numbers,boolean,number,null,undefined,symbol,BigInt
+const score=100
+const scoreValue=100.34
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+const id=Symbol('123')
+const idAnother=Symbol('123')
+console.log(id===idAnother)
+const BigNumber=123455432123432432n
+
+
+
+//reference-non-primitive
+
+
+
+//array,objects,functions
+
+const name=["moskh","kavy","manav","tanush"]
+let myobj={
+    name: "moksh",
+    age: 19,
+}
+
+
+const MyFunction = function(){
+    console.log("hello world")
+}
