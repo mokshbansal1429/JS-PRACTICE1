8d906f789ffeb768b7d488422c671e8f072d6787
mokshbansal1429 <moksh1429.becse24@chitkara.edu.in>
2025-09-02 10:16:49 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,32 @@
+const name="moksh"
+const myrepo=10
+//console.log(name + "totalrepo" + myrepo);
+console.log(`my name is ${name} and my total repos in github is ${myrepo}`);
+//1 more to define a string
+const game=new String('khelega-free-fire');
+console.log(game.__proto__);
+console.log(game.length);
+console.log(game.toUpperCase());
+console.log(game.charAt(3));
+console.log(game.indexOf('a'));
+const naam=game.substring(0,8);
+console.log(naam);
+const anothernaam=game.slice(-14,5)
+console.log(anothernaam);
+const duja="              mok  "
+console.log(duja);
+
+const dujanaam="       moksh                "
+console.log(dujanaam.trim());
+const url1="rohan@google.coi"
+console.log(url1.replace('i','m'));
+console.log(game.split('-'));
+
+
+
+
+
+
+
+
+
