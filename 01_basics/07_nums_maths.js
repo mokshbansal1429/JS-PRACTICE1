21c201063f2e46e8c7cfcbc2e2447c92f0b629f2
mokshbansal1429 <moksh1429.becse24@chitkara.edu.in>
2025-09-04 08:50:21 +0000

    numbers and maths

---
+++ 01_basics/07_nums_maths.js
@@ -0,0 +1,39 @@
+const score =100;
+console.log(score);
+const balance =new Number(1000);
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+const number=123.998;
+console.log(number.toPrecision(4));
+const number1=100000044004400;
+console.log(number1.toLocaleString('en-IN'));
+//*********************MATHS*******************************************************************
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.65));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+console.log(Math.min(3,4,5,1,-3));
+console.log(Math.max(3,4,5,7,12,1112));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random())+1);
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
